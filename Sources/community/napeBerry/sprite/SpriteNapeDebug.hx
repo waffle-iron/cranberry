0be826aec14d148d64e255f5113af104ae22ba29
Jeremy <jeremy@sourcre.am>
2017-02-09 23:44:40 -0800

    removed unused imports

--- Sources/community/napeBerry/sprite/SpriteNapeDebug.hx
+++ Sources/community/napeBerry/sprite/SpriteNapeDebug.hx
@@ -2,29 +2,9 @@ package community.napeBerry.sprite;
 
 import cranberry.sprite.Sprite;
 
-import kha.Color;
-import kha.graphics2.Graphics;
-import kha.math.FastMatrix3;
-import nape.constraint.Constraint;
-import nape.constraint.PivotJoint;
-import nape.dynamics.Arbiter;
-import nape.dynamics.ArbiterList;
-import nape.dynamics.CollisionArbiter;
-import nape.dynamics.Contact;
-import nape.geom.Vec2;
-import nape.geom.Vec2List;
-import nape.phys.Body;
-import nape.phys.BodyList;
-import nape.phys.Compound;
-import nape.shape.Edge;
-import nape.shape.EdgeList;
-import nape.shape.Polygon;
-import nape.shape.Shape;
 import nape.shape.ShapeType;
 import nape.space.Space;
-
 using kha.graphics2.GraphicsExtension;
-
 
 class SpriteNapeDebug extends Sprite
 {
@@ -40,15 +20,11 @@ class SpriteNapeDebug extends Sprite
 	/** **/
 	override public function render(framebuffer :kha.Framebuffer) : Void
 	{
-		if(space == null)
-			return;
-
 		for (body in space.bodies) {
 			framebuffer.g2.color = kha.Color.Red;
 			framebuffer.g2.drawRect(body.bounds.x, body.bounds.y, body.bounds.width, body.bounds.height);
 			
 			for (shape in body.shapes) {			
-				// var shape:Shape = body.shapes.at(j);								
 				framebuffer.g2.color = kha.Color.Magenta;
 				if (shape.type == ShapeType.POLYGON) {			
 					for (edge in shape.castPolygon.edges) {
